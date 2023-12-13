cf7012b72069974d6a2c1d9f98c7d2d1878e7fc5
Adrian <alextravc.342@gmail.com>
2023-12-13 17:43:34 -0500

    ok 1

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     serverActions: true,
   },
   images: {
-    domains: ['fmliqecnrxgzwvucipeu.supabase.co'],
+    domains: ['jkvwuvvfqyqrcyxfgfef.supabase.co'], // env - NEXT_PUBLIC_SUPABASE_URL
   },
 };
 
